d24495a63d7af9e153aab2f11ede14e00ad33885
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-08-01 00:03:12 -0400

    Add trests to code to ensure object is working correctly

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ const Gameboard = (() => {
 const boardState = Gameboard.getBoard();
 console.log(boardState); // It should output an array with 9 elements filled with null.
 
+// Outside the Gameboard object (test code)
+
+// Test the isCellEmpty method
+console.log(Gameboard.isCellEmpty(1, 1)); // It should return true (cell at row 1, column 1 is empty).
+
+// Test the placeMarker method
+const markerPlaced = Gameboard.placeMarker(0, 0, "X");
+console.log(markerPlaced); // It should return true (placement successful).
+console.log(Gameboard.getBoard()); // The board should show "X" in the top-left corner.
+
+// Test resetting the board
+Gameboard.resetBoard();
+console.log(Gameboard.getBoard()); // The board should be reset to an array with 9 elements filled with null.

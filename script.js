3e2bf70d7a9f4cf67ad55cec0e74ccf9270072cd
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-08-01 00:02:05 -0400

    Add additional methods to Gameboard object

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-// Gameboard object
 const Gameboard = (() => {
-
     let boardArray = Array(9).fill(null);
-
+  
     const getBoard = () => boardArray;
+  
+    const isCellEmpty = (row, col) => boardArray[row * 3 + col] === null;
+  
+    const placeMarker = (row, col, marker) => {
+      if (isCellEmpty(row, col)) {
+        boardArray[row * 3 + col] = marker;
+        return true; 
+      }
+      return false; 
+    };
+  
+    const resetBoard = () => {
+      boardArray = Array(9).fill(null);
+    };
   
     return {
       getBoard,
+      isCellEmpty,
+      placeMarker,
+      resetBoard,
     };
-})();
-  
+  })();
+
+
+// Outside the Gameboard object (test code)
+
+// Test the getBoard method
+const boardState = Gameboard.getBoard();
+console.log(boardState); // It should output an array with 9 elements filled with null.
+

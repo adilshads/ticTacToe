da76c4d3ebacd161430de1d1c0e4767e68084124
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-08-01 00:30:22 -0400

    Create player factory

--- script.js
+++ script.js
@@ -32,8 +32,6 @@ const Gameboard = (() => {
 const boardState = Gameboard.getBoard();
 console.log(boardState); // It should output an array with 9 elements filled with null.
 
-// Outside the Gameboard object (test code)
-
 // Test the isCellEmpty method
 console.log(Gameboard.isCellEmpty(1, 1)); // It should return true (cell at row 1, column 1 is empty).
 
@@ -45,3 +43,31 @@ console.log(Gameboard.getBoard()); // The board should show "X" in the top-left 
 // Test resetting the board
 Gameboard.resetBoard();
 console.log(Gameboard.getBoard()); // The board should be reset to an array with 9 elements filled with null.
+
+
+
+
+
+// Create the Player factory
+const createPlayer = (name, marker) => {
+  const playerName = name;
+  const playerMarker = marker;
+
+  const getName = () => playerName;
+  const getMarker = () => playerMarker;
+
+  return {
+    getName,
+    getMarker,
+  };
+};
+
+// Test usage of the Player factory
+const player1 = createPlayer("Player 1", "X");
+const player2 = createPlayer("Player 2", "O");
+
+console.log(player1.getName()); // It should output "Player 1".
+console.log(player1.getMarker()); // It should output "X".
+
+console.log(player2.getName()); // It should output "Player 2".
+console.log(player2.getMarker()); // It should output "O".
